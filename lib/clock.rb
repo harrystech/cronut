461ba6fe86a2e55506f47ab44e26d1ef5e7f0ee5
Sunny Ng <sunny@sunny-ng.com>
2014-08-07 22:26:27 -0400

    Make Clockwork config depend on TIME_ZONE instead of env variable

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,7 +5,7 @@ require './config/boot'
 require './config/environment'
 
 Clockwork.configure do |config|
-  config[:tz] = ENV['CRONUT_BASE_TIME_ZONE'] || 'America/New_York'
+  config[:tz] = TIME_ZONE || 'America/New_York'
 end
 
 every(1.minute, 'check-expired-jobs') {
