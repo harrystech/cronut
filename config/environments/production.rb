a4f454753e89220480c09acccd38a585a9646525
Andy O'Neill <andy@potatoriot.com>
2018-12-19 15:52:08 -0500

    Specify production log level (info)
    Address this deprecation warning from the logs:
    > : DEPRECATION WARNING: You did not specify a `log_level` in `production.rb`. Currently, the default value for `log_level` is `:info` for the production environment and `:debug` in all other environments. In Rails 5 the default value will be unified to `:debug` across all environments. To preserve the current setting, add the following line to your `production.rb`:
    >    config.log_level = :info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,8 +30,8 @@ Cronut::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
+  # See everything in the log
+  config.log_level = :info
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
